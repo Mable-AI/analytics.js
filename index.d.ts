22dc2a6b53a9f7e28e13113402cb1c59c2e05421
Marvin Wichmann <marvin.wichmann@unic.com>
2021-12-09 17:08:04 +0100

    fix(TS): Add typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+export declare class Analytics {
+    constructor(apiKey: any, host?: string);
+    /**
+     * Track an event for a given user.
+     *
+     * @param event The event to track
+     * @param cb Callback
+     */
+    track(event: {}, cb: any): void;
+    /**
+     * Identify event for a given user.
+     *
+     * @param event The identify data
+     * @param cb Callback
+     */
+    identify(event: {}, cb: any): void;
+}
